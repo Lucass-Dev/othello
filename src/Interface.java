260cc4ad758587594457840d6ee0ac765cfea12a
Lucass-Dev <liger.lucas33@gmail.com>
2022-12-06 19:55:38 +0100

    print menu start

--- src/Interface.java
+++ src/Interface.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Interface {
-    static final Scanner sc = new Scanner(System.in);
+
+    static Scanner sc = new Scanner(System.in);
 
 
 
@@ -9,7 +10,43 @@ public class Interface {
         System.out.println("Que voulez-vous faire ?" +
                 "\n\t-1 : Jouer" +
                 "\n\t-2 : Passer son tour" +
-                "\n\t-3 : Relire les règles");
+                "\n\t-3 : Relire les règles" +
+                "\n\t-4 : Abandonner");
         int choixJoueur = sc.nextInt();
+        while(choixJoueur >4 || choixJoueur<1){
+            System.out.println("Erreur de saisie");
+
+            System.out.println("Que voulez-vous faire ?" +
+                    "\n\t-1 : Jouer" +
+                    "\n\t-2 : Passer son tour" +
+                    "\n\t-3 : Relire les règles"+
+                    "\n\t-4 : Abandonner");
+
+            choixJoueur = sc.nextInt();
+        }
+    }
+
+    public static void printMenuPrincipal(){
+        System.out.println("Bienvenu dans Othello, que souhaitez-vous faire ?" +
+                "\n\t-1 : Jouer contre un Joueur" +
+                "\n\t-2 : Jouer contre l'IA (facile)" +
+                "\n\t-3 : Jouer contre l'IA (difficile Insh'Allah)" +
+                "\n\t-4 : Lire les règles"+
+                "\n\t-5 : Quitter");
+
+        int choixJoueur = sc.nextInt();
+
+        while(choixJoueur > 5 || choixJoueur < 1){
+            System.out.println("Erreur de saisie");
+
+            System.out.println("Bienvenu dans Othello, que souhaitez-vous faire ?" +
+                    "\n\t-1 : Jouer contre un Joueur" +
+                    "\n\t-2 : Jouer contre l'IA (facile)" +
+                    "\n\t-3 : Jouer contre l'IA (difficile Insh'Allah)" +
+                    "\n\t-4 : Lire les règles"+
+                    "\n\t-5 : Quitter");
+
+            choixJoueur = sc.nextInt();
+        }
     }
 }
